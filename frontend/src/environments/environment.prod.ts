8c553026caa5f588de746674b36f0f14251e1c13
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-10-12 16:36:11 +0100

    GitHub actions

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: "https://fullstackjavaangulartestapp.herokuapp.com/api/v1",
-  authURL: "https://fullstackjavaangulartestapp.herokuapp.com"
+  apiURL: "https://fuat-superheroes-66c6fc9630bb.herokuapp.com/api/v1",
+  authURL: "https://fuat-superheroes-66c6fc9630bb.herokuapp.com"
 };
